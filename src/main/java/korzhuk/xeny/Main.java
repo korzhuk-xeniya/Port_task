52cf9e80fb938d76da8a1b42a92486371610dd60
Ксения <korzhuk.xeniya@yandex.ru>
2024-09-09 12:00:24 +0300

    графический интерфейс реализован с помощью Swing

--- src/main/java/korzhuk/xeny/Main.java
+++ src/main/java/korzhuk/xeny/Main.java
@@ -1,6 +1,10 @@
 package korzhuk.xeny;
 
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
 import java.util.Arrays;
+import javax.swing.*;
+import java.awt.*;
 
 public class Main {
     public static void main(String[] args) {
@@ -9,5 +13,33 @@ public class Main {
         System.out.println(Arrays.toString(port.getArrayOfNumbers()));
         System.out.println(port.getUniqueGroupsElements());
 
+//    }
+//    public static void openWindowWithSwing(){
+        JFrame frame = new JFrame("Окно пользователя");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(800, 600);
+        JButton buttonOne = new JButton("Преобразовать в массив чисел");
+        frame.add(buttonOne, BorderLayout.SOUTH);
+        buttonOne.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+
+
+                JOptionPane.showMessageDialog(frame,
+                        "Массив строк преобразован в массив чисел! " +
+                                Arrays.toString(port.getArrayOfNumbers()));
+            }
+        });
+
+        JButton buttonTwo = new JButton("Вернуть всевозможные уникальные группы элементов");
+        frame.add(buttonTwo, BorderLayout.NORTH);
+        buttonTwo.addActionListener((new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane.showMessageDialog(frame, "Всевозможные уникальные упорядоченные группы элементов"
+                        + port.getUniqueGroupsElements());
+            }
+        }));
+        frame.setVisible(true);
     }
 }
