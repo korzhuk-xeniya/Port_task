3f387ca4660690c084e6b0d2a4dc7d12417feb81
Ксения <korzhuk.xeniya@yandex.ru>
2024-09-09 13:25:06 +0300

    done

--- src/main/java/korzhuk/xeny/Main.java
+++ src/main/java/korzhuk/xeny/Main.java
@@ -1,13 +1,10 @@
 package korzhuk.xeny;
 
-import java.awt.event.ActionEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.KeyEvent;
-import java.awt.event.KeyListener;
+
 import java.util.Arrays;
 import javax.swing.*;
 import java.awt.*;
-import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
